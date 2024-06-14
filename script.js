20b46858e017a849d5bc39c09e6f56a385ee1831
Sthitiprangya Biswal <sthitiprangyad252@gmail.com>
2024-06-14 20:04:26 +0530

    ToDo List

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputs= document.getElementById("input-box");
+const listContainer = document.getElementById("List-container");
+
+function Add(){
+    if(inputs.value === ""){
+        alert("plz enter task")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML= inputs.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputs.value="";
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
